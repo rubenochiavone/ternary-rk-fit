3adbb6a68120cbb3c06a80c2038df4763e1919d9
Ruben O. Chiavone <ruben.ochiavone@gmail.com>
2016-02-17 22:27:14 -0300

    Changed abstract equation model class to became a concrete class containing the residual and model impls

--- src/EquationModel.py
+++ src/EquationModel.py
@@ -1,10 +1,18 @@
-from abc import ABCMeta, abstractmethod
+from TernaryRKModel import TernaryRKModel
 
 class EquationModel:
-    __metaclass__ = ABCMeta
 
-    @abstractmethod
-    def model(self, params, data, verbose = False) : res
+    residualTag = "EquationModel::residual @"
 
-    @abstractmethod
-    def residual(self, params, data, y, verbose = False) : res
+    def model(self, params, data, verbose = False):
+        return TernaryRKModel.model(params, data, verbose)
+
+    def residual(self, params, data, exp, verbose = False):
+        calc = self.model(params, data, verbose)
+        
+        if verbose:
+            print EquationModel.residualTag, "calc", calc
+            print EquationModel.residualTag, "exp", exp
+            print EquationModel.residualTag, "residual", (exp - calc)
+        
+        return exp - calc
